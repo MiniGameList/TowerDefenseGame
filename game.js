442355d1bdb07ac7166b8b1b3f01eccc51691766
jwu5555 <492429@qq.com>
2024-02-29 14:36:54 +0800

    测试

--- game.js
+++ game.js
@@ -2,8 +2,11 @@ import './js/libs/weapp-adapter'
 import './js/libs/symbol'
 import Main from './js/main'
 
-// function f(){
-//     console.log(this);
-// }
-//
-// f();
+/*
+ function f(){
+     console.log(this);
+ }
+
+ f();
+*/
+
